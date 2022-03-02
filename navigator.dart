4c7b09ed9c9932252b42ec17c3a6a0eb74b9fea0
Gustavo Gutierrez <51726041+gustavoDeveloper597@users.noreply.github.com>
2022-03-02 09:43:22 -0600

    Delete navigator.dart

--- navigator.dart
+++
@@ -1,9 +0,0 @@
-// enviar informacion en este caso un map
- List<Map<String, dynamic>> animales = [];
-Navigator.pushNamed(context, 'detallesAnimales', arguments: animal);
-
-// recuperar informacion normalmente se usa en un widget inteligente o con estado
-
-   Widget build(BuildContext context) {
-    final Map animal = ModalRoute.of(context)!.settings.arguments as Map;
-     print(animal['titulo']);
